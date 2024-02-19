51612241b88f7e82083f476242c0c3e711049b39
Siddhartha Purwar <siddpurwar@gmail.com>
2024-02-19 21:56:43 +0530

    BUILD: Added CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models, database
 from .routers import posts, users, auth, votes
 
@@ -6,6 +8,15 @@ from .routers import posts, users, auth, votes
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
