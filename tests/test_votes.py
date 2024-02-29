a88f1ef6465ce2291925e07dea5062475f505838
Siddhartha Purwar <siddpurwar@gmail.com>
2024-02-29 22:50:09 +0530

    REFACTOR

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -2,10 +2,10 @@ from fastapi import status
 
 
 def test_create_vote(authorized_client, test_posts):
-    res = authorized_client.post("/votes", json={"post_id": 1, "add_vote": 1})
+    res = authorized_client.post("/votes", json={"post_id": 3, "add_vote": 1})
     assert res.status_code == status.HTTP_201_CREATED
 
 
 def test_delete_vote(authorized_client, test_posts):
-    res = authorized_client.post("/votes", json={"post_id": 1, "add_vote": 0})
+    res = authorized_client.post("/votes", json={"post_id": 3, "add_vote": 0})
     assert res.status_code == status.HTTP_204_NO_CONTENT
