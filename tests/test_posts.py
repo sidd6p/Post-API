0aec9603e22a4c43ead0db38051abc0ba0113a24
Siddhartha Purwar <siddpurwar@gmail.com>
2024-02-29 22:40:36 +0530

    BUILD: Added tests for all post routes

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -11,7 +11,27 @@ def test_create_post(authorized_client):
     assert res.status_code == status.HTTP_201_CREATED
 
 
+def test_get_post_by_id(client, test_posts):
+    res = client.get("/posts/3")
+    post = PostResponseBase(**res.json())
+    assert res.status_code == status.HTTP_200_OK
+
+
 def test_get_posts(client, test_posts):
     res = client.get("/posts")
     posts = PostResponseBase(**res.json()[0])
+    assert res.status_code == status.HTTP_200_OK
+
+
+def test_delete_post(authorized_client, test_posts):
+    res = authorized_client.delete("/posts/1")
+    assert res.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_update_post(authorized_client, test_posts):
+    res = authorized_client.put(
+        "/posts/2",
+        json={"title": "Updated Post 1", "content": "Updated This is Post 111"},
+    )
+    updated_post = PostResponseBase(**res.json())
     assert res.status_code == status.HTTP_200_OK
