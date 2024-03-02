d59300fb47d0a1a97e1c8797a63025adf295a141
Siddhartha Purwar <siddpurwar@gmail.com>
2024-03-03 00:08:31 +0530

    FINAL: Testing the CI/CD

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def root():
-    return {"message": "Hello Earth!"}
+    return {"message": "Hello Earth"}
 
 
 app.include_router(posts.router)
