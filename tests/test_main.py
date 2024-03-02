3e6ddc8f7a68344c308d54bcb5928f91e81bf11a
Siddhartha Purwar <siddpurwar@gmail.com>
2024-03-02 16:46:25 +0530

    Done: Final Deploy

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,4 @@ from fastapi import status
 def test_root(client):
     res = client.get("/")
     assert res.status_code == status.HTTP_200_OK
-    assert res.json().get("message") == "Hello World!"
+    assert res.json().get("message") == "Hello Earth!"
